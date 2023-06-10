19e2b6cdb6af3d208428046e7efe287b31db650b
agtmwebtoon <ss49132002@gmail.com>
2023-06-10 22:48:09 +0900

    final commit

---
+++ README.md
@@ -0,0 +1,106 @@
+# SynthView Generator
+
+This is a Flask application that allows users to upload videos and perform video processing tasks using the COLMAP and NeRF libraries. The processed videos can be downloaded by the users.
+## Prerequisites
+
+Before running the application, ensure that you have the following dependencies installed:
+- Python 3
+- Flask
+- Flask-SSE
+- Redis
+- COLMAP
+- NeRF
+
+You can install the required Python packages by running the following command:
+
+```
+
+pip install -r requirements.txt
+```
+
+
+
+Additionally, make sure you have Redis server installed and running. Update the `REDIS_URL` in the `app.config` section of the code to match your Redis server URL.
+## Usage
+1. Clone the repository:
+
+```bash
+
+git clone https://github.com/your-username/flask-video-processing.git
+```
+
+
+1. Navigate to the project directory:
+
+```bash
+
+cd flask-video-processing
+```
+
+
+1. Start the Flask application:
+
+```
+
+python app.py
+```
+
+
+
+The application will run on `http://localhost:5000` by default. 
+1. Access the application in your web browser and upload a video file. 
+2. Wait for the processing to complete. You will see log messages indicating the progress. 
+3. Once the processing is finished, you can download the processed video.
+## Project Structure
+
+The project structure is as follows:
+
+```css
+
+flask-video-processing/
+  |- scripts/
+     |- colmap2nerf.py
+     |- run.py
+  |- static/
+     |- css/
+        |- main.css
+     |- js/
+        |- main.js
+  |- templates/
+     |- index.html
+  |- app.py
+  |- base_cam.json
+  |- requirements.txt
+  |- readme.md
+```
+
+ 
+- The `scripts` directory contains the Python scripts for video processing using COLMAP and NeRF. 
+- The `static` directory contains the CSS and JavaScript files for the frontend. 
+- The `templates` directory contains the HTML template for the application's UI. 
+- The `app.py` file is the main Flask application file. 
+- The `base_cam.json` file is a configuration file for camera settings. 
+- The `requirements.txt` file lists the required Python packages. 
+- The `readme.md` file is the documentation file.
+## Contributing
+
+If you want to contribute to this project, you can follow these steps: 
+1. Fork the repository. 
+2. Create a new branch:
+
+```bash
+
+git checkout -b feature/your-feature
+```
+
+# Team Members
+
+This project is developed and maintained by the following team members:
+## Giyeol Kim
+
+He is responsible for coordinating the project, managing tasks, and ensuring timely completion of milestones. He also oversees the overall development process and ensures effective communication within the team.
+## Minhyung Lee
+
+He is responsible for designing and implementing the server-side logic of the application. He works on data processing, API development, and integration with external services.
+## Gio Kim
+He is a skilled frontend developer who handles the user interface and client-side development of the application. He is responsible for creating responsive and user-friendly interfaces using HTML, CSS, and JavaScript frameworks.
---
+++ static/js/scripts.js
@@ -0,0 +1,191 @@
+const dropZone = document.getElementById('drop-zone');
+const videoImg = document.getElementById('video-img');
+const videoPlayer = document.getElementById('video-player');
+const uploadButton = document.getElementById('upload-button');
+const trainingButton = document.getElementById('training');
+const dropContainer = document.getElementById('drop-container');
+const generatedVideo = document.getElementById('generated-video');
+const progressIndicator = document.getElementById('progress-indicator');
+generatedVideo.style.display = 'none';
+
+var video_URL = "";
+var file;
+
+document.addEventListener('DOMContentLoaded', function() {
+  var source = new EventSource("/stream");
+  source.onmessage = function(event) {
+    console.log(event);
+    var logElement = document.getElementById("response-text");
+    logElement.innerHTML += event.data + "<br>";
+  };
+});
+
+dropZone.addEventListener('dragover', (event) => {
+    event.preventDefault();
+    dropZone.style.backgroundColor = '#f1f1f1';
+});
+
+dropZone.addEventListener('dragleave', (event) => {
+    event.preventDefault();
+    dropZone.style.backgroundColor = '';
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var updateButton = document.getElementById('update-button');
+	var videoElement = document.getElementById('video-element');
+
+    updateButton.addEventListener('click', function() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/get-dummy-text', true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+                var dummyTextElement = document.getElementById('dummy-text');
+                dummyTextElement.textContent = response.dummy_text;
+            }
+        };
+        xhr.send();
+    });
+});
+
+
+dropZone.addEventListener('drop', (event) => {
+    event.preventDefault();
+
+    dropZone.style.backgroundColor = '';
+    const files = event.dataTransfer.files;
+
+    if (files.length > 0 && files[0].type.startsWith('video/')) {
+        const fileReader = new FileReader();
+        file = files[0];
+        fileReader.readAsDataURL(files[0]);
+        fileReader.onload = () => {
+            // Hide the image element
+            videoImg.classList.add('hidden');
+            // Show the video element and set its source to the dropped file
+            videoPlayer.classList.remove('hidden');
+            videoPlayer.src = fileReader.result;
+            video_URL = fileReader.result;
+			dropContainer.remove();
+        }
+		videoImg.remove();
+
+    }
+	else document.getElementById('drop-container').style.display = 'block';
+
+});
+
+function showProgressIndicator() {
+  if (progressIndicator) {
+    progressIndicator.style.display = 'block';
+  }
+}
+
+function hideProgressIndicator() {
+  if (progressIndicator) {
+    progressIndicator.style.display = "none";
+  }
+}
+
+
+// Hide the video container initially
+videoPlayer.classList.add('hidden');
+
+
+uploadButton.addEventListener('click', function() {
+  const formData = new FormData();
+  formData.append('video', file);
+	showProgressIndicator();
+  // Send the video file to the server
+    fetch("/upload-video", {
+      method: "POST",
+      body: formData,
+    })
+      .then(function (response) {
+      if (response.ok) {
+        return response.blob();
+      } else {
+        throw new Error('Upload failed');
+      }
+    })
+    .then(function (blob) {
+      // Hide the progress indicator
+      hideProgressIndicator();
+
+      // Create a temporary URL for the videoA blob
+      const videoUrl = URL.createObjectURL(blob);
+
+      // Display the generated video
+      const videoElement = document.getElementById("generated-video");
+      videoElement.src = videoUrl;
+      videoElement.style.display = "block";
+    })
+    .catch(function (error) {
+      // Hide the progress indicator
+      hideProgressIndicator();
+      console.error("Error:", error);
+    });
+});
+
+trainingButton.addEventListener('click', () => {
+    fetch('/training', {
+    method: 'POST',
+    body: ""
+  }).then(response => {
+    console.log('Video uploaded successfully');
+    console.log(response);
+  }).catch(error => {
+      console.log(formData);
+    console.error('Error uploading video:', error);
+  });
+})
+
+/*
+document.getElementById("upload-button").addEventListener("click", function () {
+  var fileInput = document.createElement("input");
+  fileInput.type = "file";
+  fileInput.accept = "video/*";
+  fileInput.click();
+
+  fileInput.addEventListener("change", function () {
+    var file = fileInput.files[0];
+    var formData = new FormData();
+    formData.append("file", file);
+
+    // Show the progress indicator
+
+
+    // Send the video file to the server
+    fetch("/upload-video", {
+      method: "POST",
+      body: formData,
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // Hide the progress indicator
+        hideProgressIndicator();
+
+        // Display the generated video
+        var videoElement = document.getElementById("generated-video");
+        videoElement.src = data.videoUrl;
+        videoElement.style.display = "block";
+      })
+      .catch(function (error) {
+        // Hide the progress indicator
+        hideProgressIndicator();
+        console.error("Error:", error);
+      });
+  });
+});
+*/
+
+document.addEventListener('DOMContentLoaded', function() {
+  var source = new EventSource("/stream");
+  source.onmessage = function(event) {
+    console.log(event);
+    var logElement = document.getElementById("response-text");
+    logElement.innerText = event.data;
+  };
+});
